15e9060b0e74868c399df63df7a491135d3d7036
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-10-08 21:23:48 +0200

    Fixed sync

--- sync/synchronize.go
+++ sync/synchronize.go
@@ -3,6 +3,8 @@ package sync
 import (
 	"context"
 	"log"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/jomei/notionapi"
@@ -23,32 +25,8 @@ func Sync(force bool) {
 			log.Fatalf("Error checking connection: %v", err)
 		}
 		if check {
-			checkTasksArchivedOnNotion(connection)
-			databaseId := notionapi.DatabaseID(connection.NotionDatabase.ID)
-			items, _ := auth.NotionClient.Database.Query(context.Background(), databaseId, nil)
-			notion.CreateProp(*connection.NotionDatabase, keys.TASK_ID_KEY, "rich_text")
-			for _, item := range items.Results {
-				tasksId := notion.GetStringValueFromProperty(item.Properties[keys.TASK_ID_KEY])
-				if tasksId == "" {
-					google.New(models.ConnectedTask{
-						Notion:     &item,
-						Task:       nil,
-						Connection: &connection,
-					})
-				} else {
-					task, err := auth.TasksService.Tasks.Get(connection.TasksList.Id, tasksId).Do()
-					connectedTask := models.ConnectedTask{
-						Notion:     &item,
-						Task:       task,
-						Connection: &connection,
-					}
-					if err != nil {
-						notion.UpdateValueFromProp(&item, viper.GetString(keys.NOTION_STATUS_KEY), viper.GetString(keys.NOTION_DONE_STATUS_VALUE))
-					} else {
-						update(connectedTask, force)
-					}
-				}
-			}
+			itemsId := syncFromNotion(connection, force)
+			syncFromGoogle(connection, force, itemsId)
 		}
 	}
 }
@@ -66,21 +44,52 @@ func checkConnection(connection models.Connection, force bool) (bool, error) {
 	return notionTime.Equal(googleTime), nil
 }
 
-func checkTasksArchivedOnNotion(conn models.Connection) {
-	pages, _ := auth.NotionClient.Database.Query(context.Background(), notionapi.DatabaseID(conn.NotionDatabase.ID), nil)
-	tasks, _ := auth.TasksService.Tasks.List(conn.TasksList.Id).Do()
-	tasksId := make([]string, len(tasks.Items))
-	for i, task := range tasks.Items {
-		tasksId[i] = task.Id
+func syncFromNotion(connection models.Connection, force bool) []string {
+	itemsId := make([]string, 0)
+	databaseId := notionapi.DatabaseID(connection.NotionDatabase.ID)
+	items, _ := auth.NotionClient.Database.Query(context.Background(), databaseId, nil)
+	notion.CreateProp(*connection.NotionDatabase, keys.TASK_ID_KEY, "rich_text")
+	for _, item := range items.Results {
+		itemsId = append(itemsId, item.ID.String())
+		tasksId := notion.GetStringValueFromProperty(item.Properties[keys.TASK_ID_KEY])
+		if tasksId == "" {
+			google.New(models.ConnectedTask{
+				Notion:     &item,
+				Task:       nil,
+				Connection: &connection,
+			})
+		} else {
+			task, err := auth.TasksService.Tasks.Get(connection.TasksList.Id, tasksId).Do()
+			connectedTask := models.ConnectedTask{
+				Notion:     &item,
+				Task:       task,
+				Connection: &connection,
+			}
+			if err != nil {
+				notion.UpdateValueFromProp(&item, viper.GetString(keys.NOTION_STATUS_KEY), viper.GetString(keys.NOTION_DONE_STATUS_VALUE))
+			} else {
+				update(connectedTask, force)
+			}
+		}
 	}
-	for _, page := range pages.Results {
-		taskId, _ := notion.GetProp(page, keys.TASK_ID_KEY)
-		if taskId != "" {
-			for i, id := range tasksId {
-				if id == taskId {
-					tasksId = append(tasksId[:i], tasksId[i+1:]...)
-					break
+	return itemsId
+}
+
+func syncFromGoogle(connection models.Connection, force bool, itemsId []string) {
+	tasks, _ := auth.TasksService.Tasks.List(connection.TasksList.Id).Do()
+	for _, task := range tasks.Items {
+		if !slices.Contains(itemsId, task.Id) {
+			if strings.Contains(task.Notes, "https://www.notion.so/") {
+				_, err := google.SetDone(*connection.TasksList, task)
+				if err != nil {
+					log.Fatalf("Error setting task done: %v", err)
 				}
+			} else {
+				notion.New(models.ConnectedTask{
+					Notion:     nil,
+					Task:       task,
+					Connection: &connection,
+				})
 			}
 		}
 	}
